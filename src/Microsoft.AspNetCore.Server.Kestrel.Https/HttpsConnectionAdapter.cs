aa69d75a591e196bd5452c38ae375833e98ceb11
Stephen Halter <halter73@gmail.com>
2017-04-26 15:38:09 -0700

    Make IAdaptedConnection disposable

--- src/Microsoft.AspNetCore.Server.Kestrel.Https/HttpsConnectionAdapter.cs
+++ src/Microsoft.AspNetCore.Server.Kestrel.Https/HttpsConnectionAdapter.cs
@@ -155,6 +155,11 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Https
 
                 requestFeatures.Get<IHttpRequestFeature>().Scheme = "https";
             }
+
+            public void Dispose()
+            {
+                _sslStream.Dispose();
+            }
         }
 
         private class ClosedAdaptedConnection : IAdaptedConnection
@@ -162,6 +167,10 @@ namespace Microsoft.AspNetCore.Server.Kestrel.Https
             public Stream ConnectionStream { get; } = new ClosedStream();
 
             public void PrepareRequest(IFeatureCollection requestFeatures)
+            {
+            }
+
+            public void Dispose()
             {
             }
         }
